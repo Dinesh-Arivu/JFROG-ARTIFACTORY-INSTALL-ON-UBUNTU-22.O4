f60f82259f4ad594a637980f40c86439fefed42c
DINESH THIYAGARAJAN <dineshthiyagu1097@gmail.com>
2025-09-09 15:32:22 +0530

    Create jfrogartifactoryinstall.sh

---
+++ jfrogartifactoryinstall.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+sudo apt update
+
+# Fetch and add the JFrog public GPG key
+wget -qO - https://releases.jfrog.io/artifactory/api/gpg/key/public | sudo apt-key add -
+
+# Determine your distribution
+DISTRO=$(lsb_release -cs)
+
+# Add JFrog Artifactory repository
+echo "deb https://releases.jfrog.io/artifactory/artifactory-debs ${DISTRO} main" | sudo tee -a /etc/apt/sources.list
+
+# Update package lists
+sudo apt-get update
+
+# Install JFrog Artifactory Community Edition (C++)
+sudo apt-get install jfrog-artifactory-cpp-ce -y
